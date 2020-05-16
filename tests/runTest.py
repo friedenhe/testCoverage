721d09a061c8d3a9235b3713b88b25fdcc850ffc
Ping He <friedenhe@gmail.com>
2020-05-16 15:24:10 -0400

    Create runTest.py

---
+++ tests/runTest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+DAFoam run script for the curved cube case
+"""
+
+# =================================================================================================
+# Imports
+# =================================================================================================
+from pyTestRun import pyTestRun
+
+test = pyTestRun()
+test.init()
+test.run()
+
